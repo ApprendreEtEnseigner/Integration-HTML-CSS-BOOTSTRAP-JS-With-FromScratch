dc1b1be4376c76a8d8dd2be439b56b114df99202
ApprendreEtEnseigner <111628262+ApprendreEtEnseigner@users.noreply.github.com>
2023-05-28 10:31:02 +0200

    Debut-implementation-bibliotheque-typed

---
+++ script.js
@@ -0,0 +1,6 @@
+//! TYPED
+var typed = new Typed('.typed', {
+    strings: ['agitateur', 'provocataire', 'agile', 'dynamique', 'dynamique', 'dynamique', 'dynamique'],
+    typeSpeed: 40,
+    backSpeed: 0
+  });
